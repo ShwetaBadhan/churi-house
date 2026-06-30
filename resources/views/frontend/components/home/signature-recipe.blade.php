3a653f54b1d314a26503a81cfe03b6ffce100c74
gracychadha <chadhagracy8@gmail.com>
2026-06-30 18:17:44 +0530

    churi house updates

--- resources/views/frontend/components/home/signature-recipe.blade.php
+++ resources/views/frontend/components/home/signature-recipe.blade.php
@@ -9,8 +9,8 @@
                     <div class="ch-counter-icon">
                         <i class="fa-solid fa-store"></i>
                     </div>
-                    <h3><span class="ch-counter" data-count="50">0</span>+</h3>
-                    <p> Signature Recipes</p>
+                    <h3><span class="ch-counter" data-count="15">0</span>+</h3>
+                    <p> Growing Outlets</p>
                 </div>
             </div>
 
@@ -19,8 +19,8 @@
                     <div class="ch-counter-icon">
                         <i class="fa-solid fa-mug-hot"></i>
                     </div>
-                    <h3><span class="ch-counter" data-count="20">0</span>K+</h3>
-                    <p> Satisfied Guests</p>
+                    <h3><span class="ch-counter" data-count="50">0</span>K+</h3>
+                    <p> Signature Recipes</p>
                 </div>
             </div>
 
@@ -29,8 +29,8 @@
                     <div class="ch-counter-icon">
                         <i class="fa-solid fa-city"></i>
                     </div>
-                    <h3><span class="ch-counter" data-count="100">0</span>+</h3>
-                    <p>Team Members</p>
+                    <h3><span class="ch-counter" data-count="20">0</span>K+</h3>
+                    <p>Satisfied Guests</p>
                 </div>
             </div>
 
@@ -39,8 +39,8 @@
                     <div class="ch-counter-icon">
                         <i class="fa-solid fa-users"></i>
                     </div>
-                    <h3><span class="ch-counter" data-count="15">0</span>+</h3>
-                    <p>Growing Outlets</p>
+                    <h3><span class="ch-counter" data-count="100">0</span>+</h3>
+                    <p>Team Members</p>
                 </div>
             </div>
 
